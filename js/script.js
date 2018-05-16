1e1dcab75a3a8cc66ce3bfc87bef15a980c7e62e
Roman <rromanishin@ucell.com.ua>
2018-05-16 14:39:56 +0300

    update owl carousel

--- js/script.js
+++ js/script.js
@@ -172,14 +172,22 @@ $(document).ready(function() {
     "use strict";
 
     $("#owl-demo").owlCarousel({
-        autoplay:true,
+        // autoplay:true,
         autoplayTimeout: 3000,
         loop: true,
         dots:true,
         items: 3, //10 items above 1000px browser width
-        itemsDesktop: [1370, 3], //5 items between 1000px and 901px
-        itemsDesktopSmall: [900, 2], // betweem 900px and 601px
-        itemsTablet: [600, 1], //2 items between 600 and 0
+        responsive: {
+            0: {
+                items: 1
+            },
+            900: {
+                items: 2
+            },
+            1280: {
+                items: 3
+            }
+        }
     });
 });
 
