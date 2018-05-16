ecb74a4635699bab24c3cbfb489e09d5d60fb55d
Roman <rromanishin@ucell.com.ua>
2018-05-16 18:03:50 +0300

    add ga

--- index.php
+++ index.php
@@ -10,6 +10,20 @@ $app = require('config/config.php');
 -->
 
 <head>
+    <?php
+    if(isset($app['ga'])){
+        $script =
+            '<script async src="https://www.googletagmanager.com/gtag/js?id='.$app['ga'].'"></script>
+            <script>
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag(\'js\', new Date());
+            
+              gtag(\'config\', \''.$app['ga'].'\');
+        </script>';
+        echo $script;
+    }
+    ?>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
