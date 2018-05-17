10fc169bb5dfecef763944131af39965e5f11c4c
Roman <rromanishin@ucell.com.ua>
2018-05-17 13:01:21 +0300

    add fb pixel

--- index.php
+++ index.php
@@ -23,6 +23,29 @@ $app = require('config/config.php');
         </script>';
         echo $script;
     }
+    if(isset($app['fbpixel'])){
+        $script =
+            '<!-- Facebook Pixel Code -->
+            <script>
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version=\'2.0\';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window,document,\'script\',
+            \'https://connect.facebook.net/en_US/fbevents.js\');
+             fbq(\'init\', \''.$app['fbpixel'].'\'); 
+            fbq(\'track\', \'PageView\');
+            </script>
+            <noscript>
+             <img height="1" width="1" 
+            src="https://www.facebook.com/tr?id='.$app['fbpixel'].'&ev=PageView
+            &noscript=1"/>
+            </noscript>
+<!-- End Facebook Pixel Code -->';
+        echo $script;
+    }
     ?>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
